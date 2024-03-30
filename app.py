3fbef2d40e97a7d265277e6683eec8aa533913a7
sachinbagale <sachinworld1234@gmail.com>
2024-03-30 21:58:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,31 @@ import cv2
 
 plt.style.use("ggplot")
 
+def dice_coefficients(y_true, y_pred, smooth=100):
+    y_true_flatten = K.flatten(y_true)
+    y_pred_flatten = K.flatten(y_pred)
+
+    intersection = K.sum(y_true_flatten * y_pred_flatten)
+    union = K.sum(y_true_flatten) + K.sum(y_pred_flatten)
+    return (2 * intersection + smooth) / (union + smooth)
+
+
+def dice_coefficients_loss(y_true, y_pred, smooth=100):
+    return -dice_coefficients(y_true, y_pred, smooth)
+
+
+def iou(y_true, y_pred, smooth=100):
+    intersection = K.sum(y_true * y_pred)
+    sum = K.sum(y_true + y_pred)
+    iou = (intersection + smooth) / (sum - intersection + smooth)
+    return iou
+
+
+def jaccard_distance(y_true, y_pred):
+    y_true_flatten = K.flatten(y_true)
+    y_pred_flatten = K.flatten(y_pred)
+    return -iou(y_true_flatten, y_pred_flatten)
+
 # Function to set background image
 @st.cache(allow_output_mutation=True)
 def get_base64_of_bin_file(bin_file):
