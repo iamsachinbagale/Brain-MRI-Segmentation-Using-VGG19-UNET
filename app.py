b80f5cfbf3f814b245e9ed34f6ab524028429163
sachinbagale <sachinworld1234@gmail.com>
2024-03-31 10:17:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,29 @@ from tensorflow.keras import backend as K
 from tensorflow.keras.models import load_model
 import cv2
 import requests
-from util import visualize, set_background
+
+def set_background(image_file):
+    """
+    This function sets the background of a Streamlit app to an image specified by the given image file.
+
+    Parameters:
+        image_file (str): The path to the image file to be used as the background.
+
+    Returns:
+        None
+    """
+    with open(image_file, "rb") as f:
+        img_data = f.read()
+    b64_encoded = base64.b64encode(img_data).decode()
+    style = f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/png;base64,{b64_encoded});
+            background-size: cover;
+        }}
+        </style>
+    """
+    st.markdown(style, unsafe_allow_html=True)
 
 set_background('./bg.png')
 
